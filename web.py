6a52069c77a95dbc54f7c506a525e0834d0d3639
ronaldliujr <rliujr1@babson.edu>
2023-12-08 17:40:37 -0500

    12/8 update

--- web.py
+++ web.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 
 nicknames_list = []
 players_data = []
+# matches_data = []
 
 
 @app.route("/")
@@ -37,36 +38,40 @@ def display_nicknames():
 
         if response.status_code == 200:
             data = response.json()
+            print("data:", data)
             processed_data = choose_player(data)
+            print("procssed_data:", processed_data)
             players = processed_data
+    print("final players:", players)
     return render_template("names.html", players_data=players, players=players)
 
 
 @app.route("/stats", methods=["GET", "POST"])
 def last_matches():
+    matches_data = []
     if request.method == "POST":
-        player_id = request.form.get("player")
+        player_id = request.form.get("items[0][stats][Player Id]")
+        print("player id:", player_id)
         url = f"https://open.faceit.com/data/v4/players/{player_id}/games/cs2/stats"
         headers = {
             "accept": "application/json",
             "Authorization": f"Bearer {FACEIT_KEY}",
         }
         limit = 10
-
         params = {"offset": 0, "limit": limit}
 
         response = requests.get(url, headers=headers, params=params)
 
-        # player_data = player_stats(player_id, limit)
-
-        # if player_data:
-        #     processed_data = match_stats(player_data)
-        #     return render_template("matches.html", player_data=processed_data)
         if response.status_code == 200:
             data = response.json()
+            print("Raw player_stats", data)
             player_data = player_stats(player_id, limit)
+            print("processed player_data", player_data)
             processed_data = match_stats(player_data)
-    return render_template("matches.html", player_data=processed_data)
+            print("processed_data:", processed_data)
+            matches_data.append(processed_data)
+    print("final matches data:", matches_data)
+    return render_template("matches.html")
 
 
 if __name__ == "__main__":
